a4c07adb743d94332f0af6b19268906a3e5a4362
asathoor <asathoor@gmail.com>
2025-10-09 17:23:18 +0200

    Dagens kode

--- liste-med-flere-objekter/js/js.js
+++ liste-med-flere-objekter/js/js.js
@@ -1,33 +1,54 @@
 let trolde = [
   {
     navn: 'Grum',
-    vaaben: ['Stridshammer', 'Armbrøst', 'Teske', 'Sut','Spegepølse','Daggert'],
+    vaaben: ['Stridshammer', 
+      'Armbrøst', 
+      'Teske', 
+      'Sut',
+      'Spegepølse',
+      'Daggert'],
     billede: 'troldHanTransp.png'
   },
   {
     navn: 'Similine',
-    vaaben: ['Snor', 'Økse', 'Kølle', 'Mjøddunk'],
+    vaaben: ['Snor', 'Økse', 'Kølle', 'Mjøddunk','Pisk','Vingummibamse'],
+    billede: 'troldHunTransp.png'
+  },
+  {
+    navn: 'Bjarne',
+    vaaben: ['Snor', 'Økse', 'Kølle', 'Mjøddunk','Pisk','Vingummibamse'],
+    billede: 'troldHunTransp.png'
+  },
+  {
+    navn: 'Asger',
+    vaaben: ['Snor', 'Økse', 'Kølle', 'Mjøddunk','Pisk','Vingummibamse'],
+    billede: 'troldHunTransp.png'
+  },
+  {
+    navn: 'Björk',
+    vaaben: ['Snor', 'Økse', 'Kølle', 'Mjøddunk','Pisk','Vingummibamse'],
     billede: 'troldHunTransp.png'
   }
 ];
 
-// Vi skriver alle trolde og deres våben ud
+// Vi bruger denne variabel til at gemme vores html til den er klar
 let output = "";
 
-// For of loopet er ret nemt at bruge
+// LOOP: FOR ... OF
 // en variabel for en enkelt instans af objektet defineres "let trol"
 // "of" henviser til objektet "trolde"
 // Det som så skal ske står i de krøllede parenteser
+
 for (let trold of trolde) {
 
   // Loopet henter hver enkelt trold ved hjælp af objektets egenskaber
-  // Læg mærke til at HTML'en skrives i "backticks"
+  // Læg mærke til at HTML'en skrives i "backticks": ` ... html ... ``
+  // I backticks kan du hente egenskaber fra objektet henstes med ${trold.EGENSKAB}
   // Egenskaberne hentes med "trold"
   // Og vi gemmer alt i en variabel der vokser og vokser med +=
   output += `<div class="troldeListe">
-
+    <!-- Troldens navn, billede og src til billede hentes frem -->
     <h2> Trold: ${trold.navn} </h2>
-
     <figure>
        <img src="images/${trold.billede}" alt="${trold.navn}">
     </figure>
@@ -35,6 +56,7 @@ for (let trold of trolde) {
     <p>Her er trolden <span class="marker"> ${trold.navn}</span>, der har disse våben med sig:</p>
     <ol>`;
 
+        // LISTEN MED VÅBEN
         // Vi looper derefter gennem våbenlisten
         // altså: der et loop mere inde i loopet
         for (let vaaben of trold.vaaben) {
@@ -45,5 +67,5 @@ for (let trold of trolde) {
     output += `</div>`; // /div
 }
 
-// Den korteste variant er at bruge id'en når der skrives
+// Nu skrives output ind i #demo
 demo.innerHTML = output;
